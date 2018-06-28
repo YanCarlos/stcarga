29470a49bc2b3fb5c47c571f79b95d742ba3ecf0
Yan <yan@Ivans-iMac.local>
2018-06-27 22:32:01 -0500

    fixed email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   layout 'mailer'
+  default from: ENV['EMAIL_USER']
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,14 @@
 class UserMailer < ApplicationMailer
-  default :from => ENV['EMAIL_USER']
 
 
   def customer_registered user
     @user = user
-    mail(to: @user.contact_email, :bcc => [@user.email, ENV['BCC_EMAIL']], subject: 'Bienvenido a nuestra compañia.')
+    mail(to: @user.contact_email, bcc: [@user.email, ENV['BCC_EMAIL']], subject: 'Bienvenido a nuestra compañia.')
   end
 
   def employee_registered user
     @employee = user
-    mail(to: @employee.email, subject: 'Bienvenido a nuestra compañia.')
+    mail(to: @employee.email, bcc: [ENV['BCC_EMAIL']], subject: 'Bienvenido a nuestra compañia.')
   end
 
 end
