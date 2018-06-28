85e102eb22689273d70ff58cab185d2b75679265
Yan <yan@Ivans-iMac.local>
2018-06-28 00:07:18 -0500

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ Myapp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { :host => ENV['APP_DOMAIN'] }
   config.action_mailer.delivery_method = :smtp
