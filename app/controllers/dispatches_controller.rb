a6e00b125d36a36a0f9af44edf4ff87444dd63b3
Yan  Carlos Marín Osorio <yancarlosmarinosorio@gmail.com>
2018-08-09 13:27:01 -0500

    header top

--- app/controllers/dispatches_controller.rb
+++ app/controllers/dispatches_controller.rb
@@ -64,8 +64,8 @@ class DispatchesController < ApplicationController
          template: "dispatches/dispatch_print.html.haml",
          encoding: 'UTF-8',
          layout: 'pdf/main.html.haml',
-         margin: {top: 20},
-         header: {html: {template: 'layouts/pdf/header.html.haml'}}
+         margin: {bottom: 20, top: 20},
+         header: {html: {template: 'layouts/pdf/header.html.haml'}},
         end
      end
   end
