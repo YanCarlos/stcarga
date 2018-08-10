94638ccbec732034e57c5958aa3b47110253ee00
Yan  Carlos Marín Osorio <yancarlosmarinosorio@gmail.com>
2018-08-10 16:01:26 -0500

    permitir fechas antiguas en contenedores

--- app/assets/javascripts/date_picker_instance.js
+++ app/assets/javascripts/date_picker_instance.js
@@ -1,6 +1,3 @@
 $(document).on('turbolinks:load', function(){
   $.fn.datepicker.defaults.format = "dd/mm/yyyy";
-  $.fn.datepicker.defaults.startDate= '-3d';
-  $.fn.datepicker.defaults.changeMonth: true;
-  $.fn.datepicker.defaultschangeYear: true;
 });
